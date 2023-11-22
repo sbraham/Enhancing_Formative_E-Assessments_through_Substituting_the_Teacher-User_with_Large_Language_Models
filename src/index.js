dc8c5f8d2055537e000410bcc696bd6bf039d43b
Samuel Braham <SXB1567@student.bham.ac.uk>
2023-11-22 13:33:25 +0000

    reconfiguring file paths

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ console.log(`index.js loaded`);
 // If the user is logged in, it will redirect to the user dashboard
 // Otherwise, it will redirect to the login page
 
-import { auth } from './firebase/firebase-config.js';
+import { auth } from './firebase/config.js';
 import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js';
 
 onAuthStateChanged(auth, (user) => {
