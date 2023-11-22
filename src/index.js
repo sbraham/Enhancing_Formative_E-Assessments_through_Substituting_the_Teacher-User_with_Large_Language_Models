d21da863a25d94990bb373be22163292ec47e934
Samuel Braham <SXB1567@student.bham.ac.uk>
2023-11-22 11:21:38 +0000

    Configuring index page to reroute based on atuthentication status

---
+++ src/index.js
@@ -0,0 +1,25 @@
+console.log(`index.js loaded`);
+
+// This is the code that will run when the page loads
+// It will check if the user is logged in or not
+// If the user is logged in, it will redirect to the user dashboard
+// Otherwise, it will redirect to the login page
+
+import { auth } from './firebase/firebase-config.js';
+import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js';
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is signed in.
+        console.log("User is logged in:", user);
+
+        // Redirect to the chat page
+        window.location.href = `./pages/login/login.html`
+    } else {
+        // No user is signed in.
+        console.log("User is not logged in");
+
+        // Redirect to the login page
+        window.location.href = `./pages/dashboard/dashboard.html`
+    }
+});
