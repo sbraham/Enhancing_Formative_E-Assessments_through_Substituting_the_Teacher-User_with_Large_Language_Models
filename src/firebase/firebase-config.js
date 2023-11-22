84f5e84384c05da23a85550b18d544523a2128a9
Samuel Braham <SXB1567@student.bham.ac.uk>
2023-11-22 11:06:42 +0000

    Committing Firebase Integration

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,20 @@
+console.log("firebase-config.js loaded");
+
+/* Importing Firebase SDK from the CDN (Content Delivery Network) */
+import * as firebase from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js';
+import * as authenticator from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js';
+
+/* Configuring Firebase SDK (Software Development Kit) */
+const firebaseConfig = {
+    apiKey: "AIzaSyDYs62LawEuFZ4zqox4rs3o_rD2fpRmeXc",
+    authDomain: "sxb1567-diss.firebaseapp.com",
+    projectId: "sxb1567-diss",
+    storageBucket: "sxb1567-diss.appspot.com",
+    messagingSenderId: "851005068985",
+    appId: "1:851005068985:web:76c8d5b4fb069bc8c6ad68",
+    measurementId: "G-W66SC8SGKH"
+};
+
+/* Initializing Firebase */
+export const app = firebase.initializeApp(firebaseConfig);
+export const auth = authenticator.getAuth(app);
