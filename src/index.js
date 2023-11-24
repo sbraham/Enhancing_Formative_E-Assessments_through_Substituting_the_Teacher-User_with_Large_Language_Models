47edb7de2ab42e038ff311facaf961177c33c721
Samuel Braham <SXB1567@student.bham.ac.uk>
2023-11-24 11:24:36 +0000

    Merging the login/logout. change based on status

--- src/index.js
+++ src/index.js
@@ -6,26 +6,8 @@ import { logout } from "./firebase/auth-helper.js";
 /* Fetching elements front HTML */
 const main = document.getElementById('main');
 const dashboardButton = document.getElementById('dashboard');
-const loginButton = document.getElementById('login');
-const logoutButton = document.getElementById('logout');
+const accountButton = document.getElementById('account');
 const loginStatus = document.getElementById('loginStatus');
-
-/* Adding event listeners to buttons */
-dashboardButton.addEventListener("click", async () => {
-    console.log(`Dashboard button clicked`);
-    main.src = "./pages/dashboard/dashboard.html";
-});
-
-loginButton.addEventListener("click", async () => {
-    console.log(`Log in button clicked`);
-    main.src = "./pages/login/login.html";
-});
-
-logoutButton.addEventListener("click", async () => {
-    console.log(`Logout button clicked`);
-    await logout();
-    main.src = "./pages/login/login.html";
-});
 
 /* Adding event listener to auth state change */
 import { auth } from '../../firebase/config.js';
@@ -36,11 +18,26 @@ onAuthStateChanged(auth, (user) => {
         // User is signed in.
         console.log("User is logged in:", user);
 
-        loginStatus.innerText = `Logged in as ${user.email}`;
+        accountButton.innerText = `Log Out`;
+        loginStatus.innerText = `🟩: Logged in as ${user.email}`;
     } else {
         // No user is signed in.
         console.log("User is not logged in");
 
-        loginStatus.innerText = `Not logged in`;
+        accountButton.innerText = `Log In`;
+        loginStatus.innerText = `🟥: Not logged in`;
     }
+});
+
+/* Adding event listeners to buttons */
+dashboardButton.addEventListener("click", async () => {
+    console.log(`Dashboard button clicked`);
+    main.src = "./pages/dashboard/dashboard.html";
+});
+
+accountButton.addEventListener("click", async () => {
+    console.log(`Account button clicked`);
+    console.log(`Loggin out...`);
+    await logout();
+    main.src = "./pages/login/login.html";
 });
